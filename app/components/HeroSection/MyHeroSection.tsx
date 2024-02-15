5fe614e435a2626b2f8d1532c5c643d960ef24fa
Mohamed Rafik <71353030+BounaderMedRafik@users.noreply.github.com>
2024-02-15 10:30:23 +0100

    2/15/2024 <3> commit
    i added spline 3d hover object in the project/HeroSect

--- app/components/HeroSection/MyHeroSection.tsx
+++ app/components/HeroSection/MyHeroSection.tsx
@@ -1,7 +1,12 @@
+"use client";
 import React from "react";
-
+import Spline from "@splinetool/react-spline";
 const MyHeroSection = () => {
-  return <div>MyHeroSection</div>;
+  return (
+    <div className="w-full h-screen">
+      <Spline scene="https://prod.spline.design/yPTWQQOqP6L0Se0w/scene.splinecode" />
+    </div>
+  );
 };
 
 export default MyHeroSection;
