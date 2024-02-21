4b5304ea7def3a6a4fd8ad2ed6c02803d568fa07
Mohamed Rafik <71353030+BounaderMedRafik@users.noreply.github.com>
2024-02-21 12:26:54 +0100

    2/21/2024 <3> commit
    i started logic and looks of sidebar and now went to take lunch :3 love yall

--- app/MyReactComponents/page.tsx
+++ app/MyReactComponents/page.tsx
@@ -1,7 +1,61 @@
 import React from "react";
+import { FaStarOfLife } from "react-icons/fa";
+import { MdHorizontalRule } from "react-icons/md";
+
+const links = [
+  {
+    id: 1,
+    title: "NavigationBar",
+    link: "./NavigationBar",
+    num: 3,
+    type: "reactC",
+  },
+  {
+    id: 2,
+    title: "Hero Section",
+    link: "./HeroSection",
+    num: 2,
+    type: "pageS",
+  },
+  {
+    id: 3,
+    title: "Footers",
+    link: "./Footer",
+    type: "reactC",
+    num: 4,
+  },
+];
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="w-full">
+      <div className="w-1/4 bg-slate-50 border-r h-screen p-6">
+        <div>
+          <div className="flex items-center mb-4 gap-2 text-xl font-bold">
+            <FaStarOfLife size={15} />
+            React Components
+          </div>
+          <div className="px-9">
+            {links.map((item) =>
+              item.type == "reactC" ? (
+                <a key={item.id} href={item.link}>
+                  <div className=" hover:bg-gray-100 transition-all text-sm border border-black/0 hover:border-black/5 hover:border rounded-md my-1 px-3 flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                      <MdHorizontalRule />
+                      {item.title}
+                    </div>
+                    <div className="text-xs px-1  border bg-blue-50 text-blue-600 rounded-full">
+                      {item.num}
+                    </div>
+                  </div>
+                </a>
+              ) : null
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
