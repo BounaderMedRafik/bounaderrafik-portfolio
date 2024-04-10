b50816b7057862ac3ba5dbc8c1cf9bd392ae9bfa
Mohamed Rafik <71353030+BounaderMedRafik@users.noreply.github.com>
2024-04-10 17:49:17 +0100

    cc

--- app/MyPortfolio/ProjectItemWeb.tsx
+++ app/MyPortfolio/ProjectItemWeb.tsx
@@ -85,6 +85,33 @@ const ProjectItemWeb = ({}: any) => {
           </div>
         </div>
       </a>
+      <a href="https://rebeca-store.webflow.io/" target="_blank">
+        <div className="w-full hover:bg-gray-900 hover:text-gray-100 text-gray-900 py-2 px-40 border-b transition-all duration-100">
+          <div className="text-3xl flex  items-center justify-between font-black">
+            <div className="flex gap-3 items-center">
+              <FaStarOfLife size={12} />
+              HouseHold (unfinished project)
+            </div>
+            <div className="ml-2 flex gap-0.5">
+              <div className="text-xs font-bold p-1 rounded-full  bg-slate-50 text-slate-800 border border-black/15 select-none cursor-default">
+                <FaReact size={20} />
+              </div>
+              <div className="text-xs font-bold p-1 rounded-full  bg-cyan-50 text-cyan-400 border border-black/15 select-none cursor-default">
+                <SiTailwindcss size={20} />
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-3 items-center">
+            <div className="text-sm flex gap-3">04/10/2024</div>
+            <div className="text-xs font-semibold">
+              3 days • Easy
+              <div className="w-[250px] h-1 bg-gray-100 rounded-full border">
+                <div className="w-[60%] h-full bg-green-400 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
     </>
   );
 };
